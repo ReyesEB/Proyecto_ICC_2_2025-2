8549c519fe56c1500064c3d4a2dd62f24f29082f
Yerik Vega <yerik.vega@utec.edu.pe>
2025-11-25 18:52:31 -0500

    algunas cosas

--- Part_2.py
+++ Part_2.py
@@ -1,26 +1,4 @@
-import cv2
 
-img_array = cv2.imread("dataset/0.jpeg",cv2.IMREAD_GRAYSCALE)
-
-print(img_array)
-
-nueva_imagen = cv2.resize(img_array,(8,8))
-
-print(nueva_imagen,"\n")
-
-print("invertimos la escala:\n")
-
-for i in range(8):
-    for j in range(8):
-        nueva_imagen[i,j] = 255-nueva_imagen[i,j]
-print(nueva_imagen,"\n")
-
-print("Achatamiento:\n")
-
-for i in range(8):
-    for j in range(8):
-        nueva_imagen[i,j] = (nueva_imagen[i,j]/255)*16
-print(nueva_imagen)
 
 # EJEMPLO GPT
 from sklearn.neighbors import KNeighborsClassifier
