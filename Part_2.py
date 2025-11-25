bc81b2191ef181e96eee2fb4a0e626a43ad45cfd
Eliber <rjeb.justme@gmail.com>
2025-11-25 18:44:38 -0500

    EJEMPLO GPT

--- Part_2.py
+++ Part_2.py
@@ -21,3 +21,28 @@ for i in range(8):
     for j in range(8):
         nueva_imagen[i,j] = (nueva_imagen[i,j]/255)*16
 print(nueva_imagen)
+
+# EJEMPLO GPT
+from sklearn.neighbors import KNeighborsClassifier
+
+# Datos de ejemplo (características)
+X = [
+    [1, 2],
+    [2, 3],
+    [3, 1],
+    [6, 7],
+    [7, 8],
+    [8, 6]
+]
+
+# Etiquetas de esos datos
+y = ['A', 'A', 'A', 'B', 'B', 'B']
+
+# Creamos el modelo con K=3
+modelo = KNeighborsClassifier(n_neighbors=3)
+
+# Entrenamos
+modelo.fit(X, y)
+
+# Predecimos para un punto nuevo
+print(modelo.predict([[4, 3]]))
