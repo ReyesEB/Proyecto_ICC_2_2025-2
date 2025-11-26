f783a23e284c924f69ed86c1cc36d5ebd957ac2b
Yerik Vega <yerik.vega@utec.edu.pe>
2025-11-25 22:28:28 -0500

    ya casi finalizado, faltan solo matrices

--- Part_1.py
+++ Part_1.py
@@ -10,11 +10,16 @@ dataset=datasets.load_digits()
 target = dataset["target"]
 images = dataset["images"]
 
+
 ruta = "los_numeros"
 valor_real = []
-
+los_3_cercanos = []
+resultados_ia = []
+motivo_de_la_clasificacion = []
+archivo_leido = []
 for archivo in os.listdir(ruta): #archivo tiene la forma de: "num"-img"num".png
     trans_cadena = str(archivo)
+    archivo_leido.append(archivo)
     split = trans_cadena.split("-")
     valor_real.append(int(split[0]))
     ruta_completa = os.path.join(ruta, archivo)
@@ -38,5 +43,34 @@ for archivo in os.listdir(ruta): #archivo tiene la forma de: "num"-img"num".png
             distancia = float(distancia)
             lista.append(distancia)
         return lista
-print(valor_real)
+    lista_euclidiana = distancia_euclidiana(nueva_imagen)
 
+
+    lista_temp = []
+    for _ in range(3):
+        minimo = lista_euclidiana.index(min(lista_euclidiana))
+        lista_temp.append(int(target[minimo]))
+        print(f"La distancia {_+1} es {lista_euclidiana[minimo]}")
+        print(target[minimo])
+        lista_euclidiana.remove(lista_euclidiana[minimo])
+        print(minimo)
+    los_3_cercanos.append(lista_temp)
+
+    repetido = None
+
+    for elemento in lista_temp:
+        if lista_temp.count(elemento) > 1:
+            repetido = elemento  # solo se guarda, no se corta el ciclo
+
+    if repetido is not None:
+        print("Soy la inteligencia artificial, y he detectado que el digito ingresado corresponde al numero:", repetido)
+        resultados_ia.append(repetido)
+        motivo_de_la_clasificacion.append(1)# 1 porque lo detecto de porque se repetian minimo 2
+    else:
+        print("Soy la inteligencia artificial, como no detecto 2 o mas similares, regreso el elemento mas cercano:", lista_temp[0])
+        resultados_ia.append(lista_temp[0])
+        motivo_de_la_clasificacion.append(0)# 0 porque ninguno se repetia mas de 2 veces
+
+resultado = np.column_stack((archivo_leido,valor_real, los_3_cercanos, resultados_ia, motivo_de_la_clasificacion))
+Dataframe = pd.DataFrame(resultado, columns=["Archivo","Valor real","Primer cercano","Segundo cercano","Tercer cercano","Clasificacion de la IA","Motivo de la clasificacion"])
+Dataframe.to_csv("resultados.csv",index=False)
